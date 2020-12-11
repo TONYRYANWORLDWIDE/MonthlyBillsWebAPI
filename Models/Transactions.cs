311dd0f9a591aaff82d934306d1797f3fe6cd221
Anthony Ryan <aryan@masseyservices.com>
2020-12-11 13:57:43 -0500

    added underscores to model

--- Models/Transactions.cs
+++ Models/Transactions.cs
@@ -5,24 +5,24 @@ namespace MonthlyBillsWebAPI.Models
 {
     public partial class Transactions
     {
-        public string AccountId { get; set; }
-        public string AccountOwner { get; set; }
+        public string Account_Id { get; set; }
+        public string Account_Owner { get; set; }
         public double? Amount { get; set; }
-        public string AuthorizedDate { get; set; }
+        public string Authorized_Date { get; set; }
         public string Category { get; set; }
         public string CategoryId { get; set; }
         public DateTime Date { get; set; }
-        public string IsoCurrencyCode { get; set; }
+        public string Iso_Currency_Code { get; set; }
         public string Location { get; set; }
         public string Name { get; set; }
-        public string PaymentChannel { get; set; }
-        public string PaymentMeta { get; set; }
+        public string Payment_Channel { get; set; }
+        public string Payment_Meta { get; set; }
         public string Pending { get; set; }
-        public string PendingTransactionId { get; set; }
-        public string TransactionId { get; set; }
-        public string TransactionType { get; set; }
-        public string UnofficialCurrencyCode { get; set; }
-        public string TransactionCode { get; set; }
-        public string MerchantName { get; set; }
+        public string Pending_Transaction_Id { get; set; }
+        public string Transaction_Id { get; set; }
+        public string Transaction_Type { get; set; }
+        public string Unofficial_Currency_Code { get; set; }
+        public string Transaction_Code { get; set; }
+        public string Merchant_Name { get; set; }
     }
 }
