f694e32f7a370df4325c0b5293eb52571fbdd141
Anthony Ryan <aryan@masseyservices.com>
2021-01-10 17:41:36 -0500

    Adding Date Filter

--- Controllers/TransactionsCMPController.cs
+++ Controllers/TransactionsCMPController.cs
@@ -24,57 +24,27 @@ namespace MonthlyBillsWebAPI.Controllers
         {
             _dapper = dapper;
         }
+
+        //api/TransactionsCMP?Transaction_Id=94Y6DE9o1Bsr3oZvKAzAiwvpgrkYVgfdE8Yyo
+        //or
+        //api/TransactionsCMP?@Account_id=XydAgkM6LNsVqL5a0eQKFoy6YJVn8PT4VMb9R&Transaction_Id=94Y6DE9o1Bsr3oZvKAzAiwvpgrkYVgfdE8Yyo
+        //or 
+        //api/TransactionsCMP? @Account_id = XydAgkM6LNsVqL5a0eQKFoy6YJVn8PT4VMb9R
+        //[HttpGet("id")]
         [Authorize]
         [HttpGet]
-        //public async Task<TransactionsCMP> GetTransactions()
-        public async Task<ActionResult<IEnumerable<TransactionsCMP>>> GetTransactions()
-        {
-
-            var dbPara = new DynamicParameters();
-
-            var transactions = await Task.FromResult(_dapper.GetAll<TransactionsCMP>("[billsapi].[getCMPTransactions]",
-               dbPara, commandType: CommandType.StoredProcedure));
-            return transactions;
-        }
-
-        //api/TransactionsCMP/id?Transaction_Id=94Y6DE9o1Bsr3oZvKAzAiwvpgrkYVgfdE8Yyo
-        //or
-        //api/TransactionsCMP/id?@Account_id=XydAgkM6LNsVqL5a0eQKFoy6YJVn8PT4VMb9R&Transaction_Id=94Y6DE9o1Bsr3oZvKAzAiwvpgrkYVgfdE8Yyo
-        //or 
-        //api/TransactionsCMP/id? @Account_id = XydAgkM6LNsVqL5a0eQKFoy6YJVn8PT4VMb9R
-        [HttpGet("id")]
-        public async Task<ActionResult<IEnumerable<TransactionsCMP>>> GetByIdName(string Account_Id, string Transaction_Id)
+        public async Task<ActionResult<IEnumerable<TransactionsCMP>>> GetByIdName(string Account_Id, string Transaction_Id, string Start_Date,string End_Date)
         {
 
             var dbPara = new DynamicParameters();
             dbPara.Add("account_id", Account_Id);
             dbPara.Add("transaction_id", Transaction_Id);
+            dbPara.Add("startDate" , Start_Date);
+            dbPara.Add("endDate", End_Date);
             var get_trans = await Task.FromResult(_dapper.GetAll<TransactionsCMP>("[billsapi].[getCMPTransactionsById]",
                                 dbPara, commandType: CommandType.StoredProcedure));
             return get_trans;
         }
-
-
-        //[HttpGet("{Account_Id}")]
-        //public async Task<ActionResult<IEnumerable<TransactionsCMP>>> GetById(string Account_Id)
-        //{
-        //    var dbPara = new DynamicParameters();
-        //    dbPara.Add("account_id", Account_Id);
-        //    var get_trans = await Task.FromResult(_dapper.GetAll<TransactionsCMP>("[billsapi].[getCMPTransactionsById]",
-        //                        dbPara, commandType: CommandType.StoredProcedure));
-        //    return get_trans;
-        //}
-        //[HttpGet("{Account_Id}/{Transaction_Id}")]
-        //public async Task<ActionResult<IEnumerable<TransactionsCMP>>> GetById(string Account_Id,string Transaction_Id)
-        //{
-
-        //    var dbPara = new DynamicParameters();
-        //    dbPara.Add("account_id", Account_Id);
-        //    dbPara.Add("transaction_id", Transaction_Id);
-        //    var get_trans = await Task.FromResult(_dapper.GetAll<TransactionsCMP>("[billsapi].[getCMPTransactionsById]",
-        //                        dbPara, commandType: CommandType.StoredProcedure));
-        //    return get_trans;
-        //}
 
         [HttpPut]
         public Task<TransactionsCMP> Update(TransactionsCMP data)
